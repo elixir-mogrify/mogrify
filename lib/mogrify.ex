9c2c5489c3a591b2ca28c52d2cd3b1f906c98959
Christian Kruse <cjk@defunct.ch>
2016-02-12 20:46:23 +0100

    feat: adding custom
    basically it is a wrapper around run

--- lib/mogrify.ex
+++ lib/mogrify.ex
@@ -117,6 +117,11 @@ defmodule Mogrify do
     image
   end
 
+  def custom(image, action, options \\ nil) do
+    run image.path, action, options
+    image
+  end
+
   defp run(path, option, params \\ nil) do
     args = ~w(-#{option} #{params} #{String.replace(path, " ", "\\ ")})
     System.cmd "mogrify", args, stderr_to_stdout: true
