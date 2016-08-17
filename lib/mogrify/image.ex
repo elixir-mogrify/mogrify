0410747a9c0b47ebf23de30c2e11d623c08b38e8
Andrew Shu <talklittle@gmail.com>
2016-08-16 18:58:51 -0700

    Image.frame_count struct field

--- lib/mogrify/image.ex
+++ lib/mogrify/image.ex
@@ -1,10 +1,34 @@
 defmodule Mogrify.Image do
-  defstruct path: nil,
-            ext: nil,
-            format: nil,
-            width: nil,
-            height: nil,
-            animated: false,
-            operations: [],
-            dirty: %{}
+
+  @type path        :: binary
+  @type ext         :: binary
+  @type format      :: binary
+  @type width       :: binary
+  @type height      :: binary
+  @type animated    :: boolean
+  @type frame_count :: integer
+  @type operations  :: Keyword.t
+  @type dirty       :: %{atom => any}
+
+  @type t :: %__MODULE__{
+    path:        path,
+    ext:         ext,
+    format:      format,
+    width:       width,
+    height:      height,
+    animated:    animated,
+    frame_count: frame_count,
+    operations:  operations,
+    dirty:       dirty
+  }
+
+  defstruct path:        nil,
+            ext:         nil,
+            format:      nil,
+            width:       nil,
+            height:      nil,
+            animated:    false,
+            frame_count: 1,
+            operations:  [],
+            dirty:       %{}
 end
