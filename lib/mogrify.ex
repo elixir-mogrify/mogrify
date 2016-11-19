27c240090afe9bc95faf541f5dd8d5103c01c2ac
Yako on Mac Pro <kyasuyako@gmail.com>
2016-11-19 21:42:15 +0900

    Fix to convert atom and numbers to string in the list

--- lib/mogrify.ex
+++ lib/mogrify.ex
@@ -74,9 +74,9 @@ defmodule Mogrify do
 
   defp normalize_arguments({:image_operator, params}), do: ~w(#{params})
   defp normalize_arguments({"annotate", params}), do: ~w(-annotate #{params})
-  defp normalize_arguments({"+" <> option, params}), do: ["+" <> option, params]
-  defp normalize_arguments({"-" <> option, params}), do: ["-" <> option, params]
-  defp normalize_arguments({option, params}), do: ["-" <> option, params]
+  defp normalize_arguments({"+" <> option, params}), do: ["+" <> to_string(option), to_string(params)]
+  defp normalize_arguments({"-" <> option, params}), do: ["-" <> to_string(option), to_string(params)]
+  defp normalize_arguments({option, params}), do: ["-" <> to_string(option), to_string(params)]
 
   @doc """
   Makes a copy of original image
